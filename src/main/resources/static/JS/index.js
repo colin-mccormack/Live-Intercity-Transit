605de4c047e99411ba5152aa7081930fbfe3b98c
colin_mccormack <colin.william.mccormack@gmail.com>
2022-08-23 21:58:31 -0400

    write notes for backend structure

--- src/main/resources/static/JS/index.js
+++ src/main/resources/static/JS/index.js
@@ -16,10 +16,31 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
  */
 
-const fs = require('fs')
+/*
 
-fs.readFile('stops.txt', (err, data) => {
-    if (err) throw err;
+    Backend solutions :
+        Create Database with information from VIA/ON
+        Have java backend query db
+        Have a js onload call to the api endpoint for stations
+        Format the response as an array
+        Loop over the array
+        Create stations objects (include data for station)
+        Use API calls for live data from VIA
+        Take the data returned from these API calls and create an icon to represent the location of a train
 
-    console.log(data);
-})
+ */
+
+
+/** vim: et:ts=4:sw=4:sts=4
+ * @license RequireJS 2.3.6 Copyright jQuery Foundation and other contributors.
+ * Released under MIT license, https://github.com/requirejs/requirejs/blob/master/LICENSE
+ */
+
+
+// The following does not work since files cant be blobs
+
+// const reader = new FileReader();
+//
+// reader.readAsDataURL('stops.txt')
+//
+// console.log (reader)
